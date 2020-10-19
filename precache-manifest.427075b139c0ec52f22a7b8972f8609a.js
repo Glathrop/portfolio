f3e3662da7113d1fb8a5eda852fe30c3ba6b2f4d
smardone <shaun.mardones@gmail.com>
2020-10-18 20:41:04 -0400

    Updates

---
+++ precache-manifest.427075b139c0ec52f22a7b8972f8609a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b72e6d23487a26109021e1d6690fd3d0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0d53b2f7ce3837ee55ae",
+    "url": "/portfolio/static/css/main.3b30f674.chunk.css"
+  },
+  {
+    "revision": "2b4c1b1ddd816548a162",
+    "url": "/portfolio/static/js/2.c5f5d2bb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio/static/js/2.c5f5d2bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d53b2f7ce3837ee55ae",
+    "url": "/portfolio/static/js/main.0791abb0.chunk.js"
+  },
+  {
+    "revision": "309a9e55c61ba5bff665",
+    "url": "/portfolio/static/js/runtime-main.b3df5f95.js"
+  },
+  {
+    "revision": "b7d4bafaaeca78d41b1d884804b28ade",
+    "url": "/portfolio/static/media/FoodFestival.b7d4bafa.png"
+  },
+  {
+    "revision": "542641370a0f4b4299f237b2ea5af1bd",
+    "url": "/portfolio/static/media/NoteTaker.54264137.png"
+  },
+  {
+    "revision": "b5102cadc0438b426799960aa41e572a",
+    "url": "/portfolio/static/media/PairPro.b5102cad.png"
+  },
+  {
+    "revision": "d5b0319534493c539716b8b39d488fcf",
+    "url": "/portfolio/static/media/ProjectGemini.d5b03195.png"
+  },
+  {
+    "revision": "2fd48a02fb6a33c35ba6f6912c30a4bf",
+    "url": "/portfolio/static/media/RunBuddy.2fd48a02.png"
+  },
+  {
+    "revision": "082146859d8268f4b7e5d3ace8b9eaff",
+    "url": "/portfolio/static/media/WeatherDashboard.08214685.png"
+  },
+  {
+    "revision": "4468e7d8808508794c9125afcd9fb956",
+    "url": "/portfolio/static/media/self.4468e7d8.jpg"
+  }
+]);
